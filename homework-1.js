873e33dc93cb8ed35c2b58144253d2120e35a842
Sergey <email@example.ru>
2023-10-31 16:13:11 +0400

    new file

--- homework-1.js
+++ homework-1.js
@@ -1,22 +1,13 @@
-let arr = [1, 4, true, 0, false, NaN, null, 6, 7, "a", 8, 1]
+let result = prompt("Введите значение:");
 
-let arrNull = 0;
-let arrEven = 0;
-let arrOdd = 0;
+let input = +result;
 
-for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] === "number") {
-    if (arr[i] === 0) {
-      arrNull++;
-    } else if (arr[i] % 2 === 0) {
-      arrEven++;
-    } else {
-      arrOdd++;
-    }
+if (!isNaN(input) && typeof input === 'number') {
+  if (input % 2 === 0) {
+    console.log("Число чётное");
+  } else {
+    console.log("Число нечётное");
   }
+} else {
+  console.log("Упс, кажется, вы ошиблись");
 }
-
-
-console.log ("
-console.log (
-console.log (
